2f181dfb3175ede14e08a28dca84cf52b25edd1b
ailymova <aily.mova@gmail.com>
2025-11-07 18:10:56 -0600

    Created event detail route

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,19 @@
 import EvenCard from "@/components/EvenCard";
-import ExploreBtn from "@/components/ExploreBtn";
-import { events } from "@/lib/constants";
-import { time } from "console";
+import ExploreBtn from '@/components/ExploreBtn';
+import { IEvent } from '@/database';
 
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
-export default function Page() {
+const Page = async () => {
+  const response = await fetch(`${BASE_URL}/api/events`);
+  const { events } = await response.json();
+
   return (
     <section>
-      <h1 className="text-center">The Hub for Every Dev<br /> Event You Can&apos;t Miss</h1>
+      <h1 className="text-center">
+        The Hub for Every Dev
+        <br /> Event You Can&apos;t Miss
+      </h1>
       <p className="text-center mt-5">Hackatons, Meetups, and Conferences All in One Place</p>
 
       <ExploreBtn />
@@ -16,15 +22,17 @@ export default function Page() {
         <h3>Feature Events</h3>
 
         <ul className="events">
-          {events.map((event) => (
-            <li key={event.title}>
-
-              <EvenCard {...event} />
-            </li>
-          ))}
+          {events &&
+            events.length > 0 &&
+            events.map((event: IEvent) => (
+              <li key={event.title}>
+                <EvenCard {...event} />
+              </li>
+            ))}
         </ul>
       </div>
-
     </section>
   );
-}
+};
+
+export default Page;
